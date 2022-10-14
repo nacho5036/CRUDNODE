77a33f759767d02ac291e1d55bb3e649847cc47a
nacho5036 <ssalazarm2000@alumno.ipn.mx>
2022-10-14 01:14:45 -0500

    conexionbd

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const conexion = mysql2.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,  
     database: process.env.DB_NAME,
-    port: process.env.PORT 
+    port: process.env.DB_PORT 
 });
 
 conexion.connect((error)=>{
