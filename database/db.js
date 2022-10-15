6baa81e574330ab997d27bcb4ffdd38e47f8817d
nacho5036 <ssalazarm2000@alumno.ipn.mx>
2022-10-14 22:45:54 -0500

    database

--- database/db.js
+++ database/db.js
@@ -1,11 +1,10 @@
 const mysql2 = require('mysql2');
 
 const conexion = mysql2.createConnection({ 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,  
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT 
+    host: 'localhost',
+    user: 'root',
+    password: 'pamela',  
+    database: 'cruddb'
 });
 
 conexion.connect((error)=>{
